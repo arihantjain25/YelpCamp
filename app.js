ae1b5254b77079a951ff21a6430c5e3804af83a1
arihant25 <arihant25jain@gmail.com>
2018-09-10 22:36:19 -0600

    add mongo

--- app.js
+++ app.js
@@ -6,14 +6,23 @@ const port = 3000;
 var express = require("express");
 var app = express();
 var bodyParser = require("body-parser");
+var mongoose = require("mongoose");
 var campgrounds = [
 		{name: "Salmon Creek", image: "http://oregondiscovery.com/wp-content/uploads/2017/08/DSC00750.jpg"},
 		{name: "Granite Hill", image: "https://a7807641ddbec626b320-8cf85e2b2ff02d1fa6d5834915d0924c.ssl.cf1.rackcdn.com/properties/photos/4847323_40_1531980660.jpg"},
 		{name: "Mountain Goat's Rest", image: "https://c8.alamy.com/comp/CTWAG4/two-baby-mountain-goats-at-rest-on-the-tundra-CTWAG4.jpg"}
 	]
 
+mongoose.connect("mongodb://localhost/yelp_camp");
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
+
+var campgroundSchema = new mongoose.Schema({
+	name: String,
+	image: String
+});
+
+var Campground = mongoose.model("Campground", campgroundSchema);
 
 app.get("/", function(req, res){
 	res.render("landing");
