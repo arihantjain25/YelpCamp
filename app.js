58fc7a10ee5fe905aeb139c97efb8aa8d1ae1a6e
arihant25 <arihant25jain@gmail.com>
2018-09-22 20:03:58 -0600

    add comment association

--- app.js
+++ app.js
@@ -3,69 +3,63 @@ const http = require('http');
 const hostname = '127.0.0.1';
 const port = 3000;
 
-var express = require("express");
-var app = express();
-var bodyParser = require("body-parser");
-var mongoose = require("mongoose");
-
+var express     = require("express"),
+    app         = express(),
+    bodyParser  = require("body-parser"),
+    mongoose    = require("mongoose"),
+    Campground  = require("./models/campground"),
+    seedDB      = require("./seeds")
+    
 mongoose.connect("mongodb://localhost/yelp_camp");
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
-
-var campgroundSchema = new mongoose.Schema({
-	name: String,
-	image: String,
-	description: String
-});
-
-var Campground = mongoose.model("Campground", campgroundSchema);
-
-
+seedDB();
 
 app.get("/", function(req, res){
-	res.render("landing");
+    res.render("landing");
 });
 
 app.get("/campgrounds", function(req, res){
-	//Get campgrounds from DB
-	Campground.find({}, function(err, allCampgrounds) {
-		if (err) {
-			console.log(err);
-		} else {
-			res.render("campgrounds", {campgrounds:allCampgrounds});
-		}
-	});
+    Campground.find({}, function(err, allCampgrounds){
+       if(err){
+           console.log(err);
+       } else {
+          res.render("index",{campgrounds:allCampgrounds});
+       }
+    });
 });
 
-app.post("/campgrounds", function(req, res) {
-	var name = req.body.name;
-	var image = req.body.image;
-	var desc = req.body.description;
-	var newCampground = {name: name, image: image, description: desc}
-	// Create new campground and save to DB
-	Campground.create(newCampground, function(err, newlyCreated){
-		if (err) {
-			console.log(err);
-		} else {
-			res.redirect("/campgrounds");
-		}
-	});
-	// res.redirect("/campgrounds");
+app.post("/campgrounds", function(req, res){
+    var name = req.body.name;
+    var image = req.body.image;
+    var desc = req.body.description;
+    var newCampground = {name: name, image: image, description: desc}
+    Campground.create(newCampground, function(err, newlyCreated){
+        if(err){
+            console.log(err);
+        } else {
+            res.redirect("/campgrounds");
+        }
+    });
 });
 
-app.get("/campgrounds/new", function(req, res) {
-	res.render("new.ejs");
+//NEW - show form to create new campground
+app.get("/campgrounds/new", function(req, res){
+   res.render("new.ejs"); 
+});
+
+// SHOW - shows more info about one campground
+app.get("/campgrounds/:id", function(req, res){
+    Campground.findById(req.params.id).populate("comments").exec(function(err, foundCampground){
+        if(err){
+            console.log(err);
+        } else {
+            console.log(foundCampground)
+            res.render("show", {campground: foundCampground});
+        }
+    });
 })
 
-app.get("/campgrounds/:id", function(req, res) {
-	Campground.findById(req.params.id, function(err, foundCampground) {
-		if (err) {
-			console.log(err);
-		} else {
-			res.render("show", {campground: foundCampground});
-		}
-	});
-})
 
 app.listen(port, hostname, function() {
 	console.log('The server has started at http://127.0.0.1:3000/ !');
